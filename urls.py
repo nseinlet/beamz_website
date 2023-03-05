6de73e5eba63cf66a320c1316dfc57ae8b919386
Nicolas Seinlet <nse@odoo.com>
2023-03-05 20:28:26 +0100

    [IMP] new website layout

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import views
 from django.urls import path
 
-from .views import index
+from .views import aboutus, index, pricing
 
 urlpatterns = [
-    path("", index, name="register"),
+    path("", index, name="home"),
+    path("pricing", pricing, name="pricing"),
+    path("aboutus", aboutus, name="aboutus"),
 ]
