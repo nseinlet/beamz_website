1f6b2cce31fd2a98f13fef3d414233d26f5a4262
Nicolas Seinlet <nicolas@seinlet.com>
2025-01-06 18:45:16 +0100

    [IMP] add pictures for error pages

--- error_views.py
+++ error_views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 
 
 def error_403(request, exception=None):
+    img = random.randint(1, 3)
     return render(request, 'errors/403.html')
 
 def error_404(request, exception=None):
+    img = random.randint(1, 5)
     return render(request, 'errors/404.html')
 
 def error_500(request, exception=None):
