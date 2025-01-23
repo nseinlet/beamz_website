81227765ba0c9dab283f55ba32d36d915d17841a
Nicolas Seinlet <nicolas@seinlet.com>
2025-01-23 20:04:22 +0100

    [IMP] one template for error pages

--- error_views.py
+++ error_views.py
@@ -3,14 +3,18 @@ import random
 from django.shortcuts import render
 
 
+def error_400(request, exception=None):
+    img = random.randint(1, 3)
+    return render(request, 'error.html', context={'img': img, 'code': 400}, status=400)
+
 def error_403(request, exception=None):
     img = random.randint(1, 3)
-    return render(request, 'errors/403.html')
+    return render(request, 'error.html', context={'img': img, 'code': 403}, status=403)
 
 def error_404(request, exception=None):
-    img = random.randint(1, 5)
-    return render(request, 'errors/404.html')
+    img = random.randint(1, 3)
+    return render(request, 'error.html', context={'img': img, 'code': 404}, status=404)
 
 def error_500(request, exception=None):
     img = random.randint(1, 4)
-    return render(request, 'errors/500.html', context={'img': img})
+    return render(request, 'error.html', context={'img': img, 'code': 500}, status=500)
